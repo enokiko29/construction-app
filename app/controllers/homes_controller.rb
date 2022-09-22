8d141fb90b3669eda883fa570028955ccab962fe
あむ <amu@amunoMacBook-Pro.local>
2022-09-22 17:10:22 +0900

    guest-login-2

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,9 @@
 class HomesController < ApplicationController
+  before_action :sign_in_required, only: [:show]
+  
   def index
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  get 'homes#index'
-  devise_for :users
+  root 'homes#index'
+  get  'homes/index'
+  get  'homes/show'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
