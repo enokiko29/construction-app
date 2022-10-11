52f54b676a04ee0345dc4fb52fb40907408d4fae
あむ <amu@amunoMacBook-Pro.local>
2022-10-11 21:55:02 +0900

    importmap-setting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers/component_controller"
-import "bootstrap"
-import "@popperjs/core"
+import jquery from "jquery"
+window.$ = jquery
+
+$("#micropost_image").bind("change", function() {
+    var size_in_megabytes = this.files[0].size/1024/1024;
+    if (size_in_megabytes > 5) {
+      alert("Maximum file size is 5MB. Please choose a smaller file.");
+      $("#micropost_image").val("");
+    }
+  });
